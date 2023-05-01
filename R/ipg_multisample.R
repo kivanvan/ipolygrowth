f6e69721130e9502f64610e5ab1a91a20a9bf7ef
kivanvan <jifan.wang@tufts.edu>
2023-05-01 11:05:53 -0400

    draft v01

--- R/ipg_multisample.R
+++ R/ipg_multisample.R
@@ -1,56 +1,55 @@
 #' @title Growth parameter estimates for multiple samples
 #'
 #' @description
-#' Add descriptions
+#' This function estimates growth parameters for a dataset with multiple samples. Technical replicates are allowed in the dataset.
 #'
-#' @param df.input Data contains time, measurements, and sample identifier from multiple samples. Data needs to be in long format.
-#' @param factors Sample identifier
-#' @param time Name of the time variable
-#' @param y Name of the measurement variable
-#' @param max.od.select A vector of threshold for maximum detection in exponential phase. See explanation below.
+#' @param df.input Input data containing the time and dependent variable from a single sample. Data needs to be in long format.
+#' Remove rows with missing in the dependent variable.
+#' @param factors Sample identifier(s). When exists more than one identifier, put a vector of variable names of the identifiers.
+#' @param time.name Name of the time variable
+#' @param y.name Name of the dependent variable
+#' @param max.y.select A threshold for maximum detection of dependent variable in exponential phase. Default is 0.2%.
+#' This number will be multiplied by the range of the dependent variable to establish a threshold for maximum detection.
 #'
 #' @details
-#' Explain the max.od.select
+#' The function uses the same approach to estimate growth parameters as in `ipg_singlesample()`.
 #'
-#' @return A list that contains a table of estimates, polynomial models, a table of beta coefficients, and a table of fitted values.
-#' @import dplyr stats
+#' @return A list that contains a table of estimates, polynomial models, a table of beta coefficients, and a table of fitted values, all by sample identifier(s).
+#' Growth parameters include peak growth rate, peak growth time, lag time, max y (fit), max y time (fit), and doubling time at the peak growth.
 #'
 #' @examples
-#' Add examples
+#' data <- growthrates::bactgrowth
+#' out.multisample <- ipg_multisample(data, c("strain", "conc"), "time", "value", 0.2/100)
 #'
-ipg_multisample <- function(df.input, factors, time, y, max.od.select) {
-  ls.df <- split(df.input, df.input[, factors], sep = "^")                                          # split datasets; it's separated by .
+#'
+ipg_multisample <- function(df.input, factors, time.name, y.name, max.y.select) {
+  ls.df <- split(df.input, df.input[, factors], sep = "^")                                          # split datasets; name is separated by ^
 
   df.factor <- data.frame(order = names(ls.df)) %>%
-    tidyr::separate_wider_delim(., "order", "^", names = factors)                                   # unique combination of stratified factors
+    tidyr::separate_wider_delim(., "order", "^", names = factors)                                   # unique combination of stratified factors by row
 
-  duptimes <- df.input[, c(factors, time)] %>%
-   # duptimes <- df.input %>% dplyr::select(tidyselect::all_of(vf), time) %>%
-    dplyr::distinct() %>% dplyr::group_by(dplyr::across(tidyselect::all_of(factors))) %>%
-    # dplyr::distinct() %>% dplyr::group_by(.data[[factors]]) %>%
-    dplyr::summarise(n = n())
-  duptimes <- duptimes$n                                                                            # get duplicated times for each data subset [edit]
-  df.factor2 <- df.factor %>% dplyr::slice(rep(1:nrow(.), duptimes))                                # duplicate factors for fitted value
-
-  ls.model <- ls.fit <- vector(mode = 'list', length = nrow(df.factor))                             # create lists to store model and fitted values
+  ls.model <- vector(mode = 'list', length = nrow(df.factor))                                       # create lists to store model and fitted values
   names(ls.model) <- do.call(paste, df.factor[names(df.factor)])                                    # match component name with stratified factors
 
   output.estimates <- data.frame(
-#    df.factor,
     peak.growth.rate = NA_real_, peak.growth.time = NA_real_, start.peak.growth = NA_real_,
-    fit.max.od = NA_real_, fit.max.od.time = NA_real_,doubling.time = NA_real_)                     # create a dataset to store param estimates together
+    fit.max.y = NA_real_, fit.max.y.time = NA_real_,doubling.time = NA_real_)                       # create a dataset to store param estimates together
 
   output.betas <- data.frame(
-#    df.factor,
-    B0 = NA_real_, B1 = NA_real_, B2 = NA_real_, B3 = NA_real_, B4 = NA_real_)                      # create a dataset to store beta cof together
+    B0 = NA_real_, B1 = NA_real_, B2 = NA_real_, B3 = NA_real_, B4 = NA_real_)                      # create a dataset to store beta coefficient
 
-  output.fitted <- data.frame(
-    time = NULL, fit = NULL
-  )
+  output.fitted <- data.frame(time = NULL, fit = NULL)                                              # create a dataset to store fitted values
+
+  duptimes <- df.input[, c(factors, time.name)] %>%                                                 # There will be issue when missing in y
+    dplyr::distinct() %>%
+    dplyr::group_by(dplyr::across(tidyselect::all_of(factors))) %>%
+    dplyr::summarise(n = n())
+    duptimes <- duptimes$n                                                                          # get duplicated times for each data subset
+    df.factor2 <- df.factor %>% dplyr::slice(rep(1:nrow(.), duptimes))                              # duplicate factors for fitted value
 
 
   for (i in 1:length(ls.df)) {
-    temp <- ipg_singlesample(ls.df[[i]], time, y, max.od.select[i]) # requires a vector of threshold
+    temp <- ipg_singlesample(ls.df[[i]], time.name, y.name, max.y.select[i])                        # requires a vector of threshold
     ls.model[[i]] <- temp$model
     # print(ncol(output.estimates))
     # print(temp$estimates)
@@ -62,7 +61,9 @@ ipg_multisample <- function(df.input, factors, time, y, max.od.select) {
     output.fitted <- rbind(output.fitted, temp$fitted)
   }
 
-  colnames(output.estimates) <- c("peak growth rate", "peak growth time", "start of peak growth", "max od (fit)", "max od time (fit)", "doubling time")
+
+  ## clean output ##----
+  colnames(output.estimates) <- c("Peak growth rate", "Peak growth time", "Lag time", "Max y (fit)", "Max y time (fit)", "Doubling time")
   output.estimates <- cbind(df.factor, output.estimates)
   output.betas <- cbind(df.factor, output.betas)
   output.fitted <- cbind(df.factor2, output.fitted)
---
+++ R/ipg_singlesample.R
@@ -0,0 +1,131 @@
+#' @title Growth parameter estimates for single sample
+#'
+#' @description
+#' This function estimates growth parameters for a single sample. Technical replicates are allowed in the dataset.
+#'
+#' @param df.input Input data containing the time and dependent variable from a single sample. Data needs to be in long format.
+#' @param time.name Name of the time variable
+#' @param y.name Name of the dependent variable
+#' @param max.y.select A threshold for maximum detection of dependent variable in exponential phase. Default is 0.2%.
+#' This number will be multiplied by the range of the dependent variable to establish a threshold for maximum detection.
+#'
+#' @details
+#' This function fits a 4th-degree polynomial model on input data. Peak growth time is first determined by the second derivative of model.
+#' Peak growth rate is then calculated as the first derivative at peak growth time.
+#' Doubling time at the peak growth is calculated using the equation: \eqn{\frac{ln(2)}{peak\ growth\ rate}}.
+#' The lag time is estimated as the time when the line of intercept from the polynomial model intersects with the linear line of peak growth at peak growth time.
+#' Difference in fitted value of y between two successive time intervals is compared to the selected threshold (the input percentage * range of y) sequentially to estimate the maximum of y and its time.
+#'
+#'
+#' @return A list that contains a table of estimates, the polynomial model, a table of beta coefficients, and a table of fitted values.
+#' Growth parameters include peak growth rate, peak growth time, lag time, max y (fit), max y time (fit), and doubling time at the peak growth.
+#'
+#' @examples
+#' data <- growthrates::bactgrowth
+#' df.singlesample <- data %>% dplyr::filter(strain == "D", conc == 0)
+#' out.singlesample <- ipg_singlesample(df.singlesample, "time", "value")
+#'
+#'
+ipg_singlesample <- function(df.input, time.name, y.name, max.y.select = 0.2/100) {
+  stopifnot("Y is not found in the data" = !is.null(df.input[[y.name]]))
+  stopifnot("Y must be a numeric variable" = is.numeric(df.input[[y.name]]))
+  stopifnot("Time is not found in the data" = !is.null(df.input[[time.name]]))
+  stopifnot("Time must be a numeric variable" = is.numeric(df.input[[time.name]]))
+  # if (!is.numeric(df.input[[y.name]])) {
+  #   warning("Y needs to be numeric.")
+  # } else if (!is.numeric(df.input[[time.name]])) {
+  #   warning("Time needs to be numeric.")
+  # } else {
+      gcdata <- df.input %>%
+        dplyr::mutate(time = .data[[time.name]],
+                      # time := {{time.name}},
+                      time_sq = time^2,
+                      time_cb = time^3,
+                      time_qt = time^4) %>%
+        dplyr::arrange(time)
+
+      ## polynomial model ##----
+      curve <- stats::lm(as.formula(paste(y.name, "~ time + time_sq + time_cb + time_qt")),
+                         data = gcdata)                                                             # 4th order polynomial regression (assumes independent obs)
+
+      gcdata2 <- gcdata %>%
+        dplyr::select(time, any_of(y.name)) %>%
+        stats::na.omit() %>%                                                                        # to avoid mismatch in length if any missing in y
+        dplyr::mutate(fit = stats::predict(curve)) %>%
+        dplyr::select(time, fit) %>%
+        dplyr::distinct() %>%                                                                       # create a data frame for predicted log OD and time
+        dplyr::mutate(diffOD = c(0, diff(fit, lag = 1)))                                            # difference in fitted log OD
+
+      tb.beta <- data.frame(B0 = NA_real_, B1 = NA_real_, B2 = NA_real_, B3 = NA_real_, B4 = NA_real_)
+      tb.beta$B0 <- A0 <- as.numeric(stats::coef(curve)[1])                                         # extract betas
+      tb.beta$B1 <- A1 <- as.numeric(stats::coef(curve)[2])
+      tb.beta$B2 <- A2 <- as.numeric(stats::coef(curve)[3])
+      tb.beta$B3 <- A3 <- as.numeric(stats::coef(curve)[4])
+      tb.beta$B4 <- A4 <- as.numeric(stats::coef(curve)[5])
+
+      tb.result <- data.frame(
+        peak.growth.rate = NA_real_, peak.growth.time = NA_real_, start.peak.growth = NA_real_,
+        fit.max.y = NA_real_, fit.max.y.time = NA_real_, doubling.time = NA_real_)
+
+
+      ## calculate peak growth rate ##----
+        # first derivative of 4th degree polynomial is A1 + A2 * (2 * x) + A3 * (3 * x^2) + A4 * (4 * x^3)
+        # second derivative of 4th degree polynomial is A2 * 2 + A3 * (3 * (2 * x)) + A4 * (4 * (3 * x^2))
+      x.roots <- polyroot(c(A2 * 2, A3 * (3 * (2)), A4 * (4 * (3))))                                # find roots of second derivative; peak growth times
+      x <- c(x.roots,0)                                                                             # add 0 to calculate the slope at 0
+
+      pgr <- Re(eval(A1 + A2 * (2 * x) + A3 * (3 * x^2) + A4 * (4 * x^3)))                          # calculate growth rate (slope) at peak growth times (real number only)
+      max <- max(gcdata$time, na.rm = T)
+      pgr.index <- which(pgr == max(pgr[pgr>0 & Re(x) >= 0 & Re(x) <= max]))[1]                     # choose growth time with maximum growth rate and peak growth time between the start and end point
+
+      tb.result$peak.growth.time <- pgt <- Re(x[pgr.index])                                         # peak growth time
+      tb.result$peak.growth.rate <- pgr[pgr.index]                                                  # peak growth rate at peak growth time
+      tb.result$doubling.time <-
+        Re(log(2)/eval(A1 + A2*(2*x[pgr.index]) + A3*(3*x[pgr.index]^2) + A4*(4*x[pgr.index]^3)))   # doubling time at peak growth = ln(2)/growth rate
+
+
+      ## calculate lag time (start of peak growth) ##----
+        #  the time when the line of intercept from the polynomial model intersects with the linear line of peak growth at peak growth time
+      if (tb.result$peak.growth.time == 0) {
+        tb.result$start.peak.growth <- 0
+      } else {
+        y0 <- gcdata2$fit[1]                                                                        # intercept of polynomial curve
+        y1 <- eval(expression(A0 + A1 * pgt + A2 * pgt^2 + A3 * pgt^3 + A4 * pgt^4))                # calculate predicted y at peak growth time
+        tb.result$start.peak.growth <- solve(pgr[pgr.index], pgr[pgr.index]*pgt-(y1-y0))            # solve for x0 when (y1-y0)/(pgt-x0) = pgr
+      }
+
+
+      ## calculate max y ##----
+      r <- gcdata %>% dplyr::select(any_of(y.name)) %>% range()                                     # get range of y
+      threshold <- (r[2] - r[1]) * max.y.select                                                     # calculate cutoff based on range
+
+      if (any((gcdata2$diffOD < threshold) & (gcdata2$diffOD > 0), na.rm=TRUE)) {                   # look for difference in fitted y b/w 0 and threshold
+
+        if (any(gcdata2$time[(gcdata2$diffOD < threshold) & (gcdata2$diffOD > 0)]
+                > tb.result$peak.growth.time, na.rm=TRUE)) {                                        # look for time of that larger than peak growth time
+
+          temp.time <- gcdata2$time[(gcdata2$diffOD < threshold) & (gcdata2$diffOD > 0)]            # find all times when fitted y is b/w 0 and threshold
+          temp <- temp.time[temp.time > tb.result$peak.growth.time][1]                              # select the first one that is later than the peak growth time
+          tb.result$fit.max.y <- gcdata2$fit[gcdata2$time == temp]                                  # return the corresponding fitted OD value
+
+        } else {tb.result$fit.max.y <- max(gcdata2$fit)}
+      } else {
+        tb.result$fit.max.y <- max(gcdata2$fit)
+        message("No difference smaller than the threshold. May consider a larger threshold.")}
+
+      tb.result$fit.max.y.time <- gcdata2$time[gcdata2$fit == tb.result$fit.max.y][1]               # record time of max y for all scenario
+
+
+      ## clean output ##----
+      colnames(tb.result) <- c("Peak growth rate", "Peak growth time", "Lag time", "Max y (fit)", "Max y time (fit)", "Doubling time")
+
+      output <- list(estimates = tb.result,
+                     model = curve,
+                     betas = tb.beta,
+                     fitted = gcdata2 %>% dplyr::select(time, fit))
+
+      return(output)
+
+  # }
+
+}
